58cfb61f6861ffe42ae4d90ee4ecbee9c44b1d46
Joao Pedro <joaopedrobautista@alunos.utfpr.edu.br>
2024-04-29 18:29:12 -0300

    Codigo problematico SRP

---
+++ SOLID/SRP/problematico-SRP.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Calculator {
+public:
+    void add(int a, int b) {
+
+        int res = a + b;
+
+        cout << "Resultado: " << res;
+
+    }
+};
+
+int main() {
+
+    Calculator calc;
+
+    calc.add(20,60);
+
+    
+    return 0;
+}
