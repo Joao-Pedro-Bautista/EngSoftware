70827bebfa4ea01f029d458d3c3fe8d6159f50b1
Joao Pedro <joaopedrobautista@alunos.utfpr.edu.br>
2024-04-29 18:27:05 -0300

    Codigo problematico de DIP

---
+++ SOLID/DIP/problematico-DIP.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Arquivo{
+
+    void gerarArquivo(string nomeArquivo) {
+        //codigo abstrato que gera arquivos automaticamente
+    }
+
+};
+
+
+
+
+
+
+int main(){
+
+    
+
+
+    return 0;
+}
